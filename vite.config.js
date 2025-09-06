3183104157d2a5922316e6fc1ff39a1621837df7
Nicolas <plus47as@gmail.com>
2025-09-06 12:11:47 -0300

    chore: agrega vite.config y plugin react

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Configuración para Vite 
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-  },
+  base: "/",
 });
